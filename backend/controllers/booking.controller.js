b11c36c9a81eabe15b5e170feb073c26146f10b6
Jyot <vasavajyotkumar@gmail.com>
2025-12-14 02:14:56 +0530

    solved non working functionality

--- backend/controllers/booking.controller.js
+++
@@ -1,6 +0,0 @@
-import AsyncHandler from "../utils/AsyncHandler.js";
-import ApiError from "../utils/ApiError.js";
-import ApiResponse from "../utils/ApiResponse.js";
-
-
-
--- backend/routes/booking.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { processPayment } from "../controllers/booking.controller.js";
-import authMiddleware from "../middleware/auth.middleware.js";
-
-const router = Router();
-
-router.route("/payment").post(authMiddleware, processPayment);
-
-export default router;
